d3d4afb9c3e43dda7af54639d7d4293e11519246
Lucas Chen <lucas.chen2016@gmail.com>
2021-05-31 19:26:33 -0400

    Update files

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
-var tableData = [{'Genre': ['Mystery', 'Thriller'], 'Liked': ['Shawn'], 'Rating': 8.5, 'Runtime': 112, 'Seen': ['Shawn'], 'Title': 'Rear Window'},
- {'Genre': ['Crime', 'Drama'], 'Rating': 9.0, 'Runtime': 96, 'Title': '12 Angry Men'}, ]
+var tableData = {"12345":{'Genre': ['Mystery', 'Thriller'], 'Liked': ['Shawn'], 'Rating': 8.5, 'Runtime': 112, 'Seen': ['Shawn'], 'Title': 'Rear Window'},
+ "3456":{'Genre': ['Crime', 'Drama'], 'Rating': 9.0, 'Runtime': 96, 'Title': '12 Angry Men'}}
 buildTable(tableData)
 
-function buildTable(data) {
+function buildTable(table_data) {
     var table = document.getElementById('movie-table')
-    for (i in data){
+    for (i in table_data){
         var row = `<div class="table-spacing"></div>
                     <tbody class="list-item">
                         <tr class="list-table-data">
-                            <td>${data[i].Title}</td>
-                            <td>${data[i].Runtime}</td>
-                            <td>${data[i].Genre}</td>
-                            <td>${data[i].Rating}</td>
+                            <td>${table_data[i]['Title']}</td>
+                            <td>${table_data[i]['Runtime']}</td>
+                            <td>${table_data[i]['Genre']}</td>
+                            <td>${table_data[i]['Rating']}</td>
                         </tr>
                     </tbody>`
         table.innerHTML += row
+        // println(data[i]['Title'])
     }
 }
 
