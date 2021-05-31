ad8d52346686b7070c038a59f80b28c59e395cd1
Lucas Chen <lucas.chen2016@gmail.com>
2021-05-31 15:44:02 -0400

    Update files

--- js/script.js
+++ js/script.js
@@ -1,26 +1,17 @@
-var tableData = [{'Title':'21',
-                'Runtime': 123,
-                'Genres':['Comedy','Horror'],
-                'Rating':7.0},
-                {'Title':'Alien',
-                'Runtime':123,
-                'Genres':['Action','Comedy'],
-                'Rating':8.0},
-                {'Title':'Bob',
-                'Genres':['Action'],
-                'Rating': 7}]
+var tableData = [{'Genre': ['Mystery', 'Thriller'], 'Liked': ['Shawn'], 'Rating': 8.5, 'Runtime': 112, 'Seen': ['Shawn'], 'Title': 'Rear Window'},
+ {'Genre': ['Crime', 'Drama'], 'Rating': 9.0, 'Runtime': 96, 'Title': '12 Angry Men'}, ]
 buildTable(tableData)
 
 function buildTable(data) {
-var table = document.getElementById('table-body')
-for (i in data){
-    var row = `<tr>
-                <td>${data[i].Title}</td>
-                <td>${data[i].Runtime}</td>
-                <td>${data[i].Genres}</td>
-                <td>${data[i].Rating}</td>
-            </tr>`
-    table.innerHTML += row
+    var table = document.getElementById('table-body')
+    for (i in data){
+        var row = `<tr>
+                    <td>${data[i].Title}</td>
+                    <td>${data[i].Runtime}</td>
+                    <td>${data[i].Genre}</td>
+                    <td>${data[i].Rating}</td>
+                </tr>`
+        table.innerHTML += row
     }
 }
 
